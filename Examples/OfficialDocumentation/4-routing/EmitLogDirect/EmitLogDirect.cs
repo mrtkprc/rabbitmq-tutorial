4ac95ce7f55319d4b8f5bcec17543b0d1711df87
Mert Koprucu <mert.koprucu@siemens.com>
2022-02-18 23:05:50 +0300

    Fix wrong codes.

--- Examples/OfficialDocumentation/4-routing/EmitLogDirect/EmitLogDirect.cs
+++ Examples/OfficialDocumentation/4-routing/EmitLogDirect/EmitLogDirect.cs
@@ -1,9 +1,9 @@
 ﻿using System;
+using System.Linq;
 using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
 using System.Text;
 
-class ReceiveLogsDirect
+class EmitLogDirect
 {
     public static void Main(string[] args)
     {
@@ -12,37 +12,15 @@ class ReceiveLogsDirect
         using(var channel = connection.CreateModel())
         {
             channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
-            // declare a server-named queue
-            var queueName = channel.QueueDeclare(queue: "").QueueName;
 
-            if (args.Length < 1)
-            {
-                Console.Error.WriteLine("Usage: {0} [info] [warning] [error]", Environment.GetCommandLineArgs()[0]);
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
-                Environment.ExitCode = 1;
-                return;
-            }
-
-            foreach(var severity in args)
-            {
-                channel.QueueBind(queue: queueName, exchange: "direct_logs", routingKey: severity);
-            }
-
-            Console.WriteLine(" [*] Waiting for messages.");
-
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (model, ea) =>
-            {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                var routingKey = ea.RoutingKey;
-                Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
-            };
-            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
-
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
+            var severity = (args.Length > 0) ? args[0] : "info";
+            var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "direct_logs", routingKey: severity, basicProperties: null, body: body);
+            Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
         }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
     }
 }
--- Examples/OfficialDocumentation/4-routing/ReceiveLogDirect/ReceiveLogDirect.cs
+++ Examples/OfficialDocumentation/4-routing/ReceiveLogDirect/ReceiveLogDirect.cs
@@ -3,28 +3,41 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
-class ReceiveLogs
+class ReceiveLogsDirect
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
         var factory = new ConnectionFactory() { HostName = "localhost" };
         using(var connection = factory.CreateConnection())
         using(var channel = connection.CreateModel())
         {
-            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
-
+            channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
             // declare a server-named queue
             var queueName = channel.QueueDeclare(queue: "").QueueName;
-            channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
 
-            Console.WriteLine(" [*] Waiting for logs.");
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Usage: {0} [info] [warning] [error]", Environment.GetCommandLineArgs()[0]);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            foreach(var severity in args)
+            {
+                channel.QueueBind(queue: queueName, exchange: "direct_logs", routingKey: severity);
+            }
+
+            Console.WriteLine(" [*] Waiting for messages.");
 
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) =>
             {
-                byte[] body = ea.Body.ToArray();
+                var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine(" [x] {0}", message);
+                var routingKey = ea.RoutingKey;
+                Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
             };
             channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
 
